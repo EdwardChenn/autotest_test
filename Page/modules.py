8b0e3bb51f6f6f41358a88207e608046bc29d5f2
EdwardChenn <1285712939@qq.com>
2020-06-17 18:41:36 +0800

    登录

---
+++ Page/modules.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+def login_by_phone(phone,pwd):
+    driver = webdriver.Chrome()     #打开谷歌浏览器
+    phone = ''
+    pwd = ''
+    driver.get("url")
+    driver.maximize_window()
+    WebDriverWait(driver,10,0.2).until(lambda x: x.find_element_by_css_selector("span.item.login-by-phone")).click()
+    # elem_login_by_phone = driver.find_element_by_css_selector("span.item.login-by-phone")
+    # driver.find_element_by_css_selector("span.item.login-by-phone").click()
+    elem_username = driver.find_element_by_css_selector("input.el-input__inner")
+    elem_username.send_keys(phone)
+    elem_kwd = driver.find_element_by_css_selector("input.el-input__inner[type~='password']")
+    elem_kwd.send_keys(pwd)
+    elem_kwd.send_keys(Keys.ENTER)
